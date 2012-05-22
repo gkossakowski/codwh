a6ddde056535ab3c289933378cba4eb8e23e339c
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2012-05-22 12:59:50 +0200

    Initial implementation of striping and scheduling.
    Implemented cutting query into stripes. See
    comment on stripeOperation method for details.
    
    Also, implemented basic scheduling algorithm
    that divides available nodes into buckets of
    size nodes/stripes and assign stripe processing
    to each bucket. This is an initial strategy
    and is going to be refined soon.

--- src/distributed/node.cc
+++ src/distributed/node.cc
@@ -66,19 +66,159 @@ void WorkerNode::run() {
  };
  return ;
 }
+/*
+ * Cuts query into stripes.
+ *
+ * Each stripe is created whenever there's group by operation
+ * encountered in query tree.
+ *
+ * Each stripe (apart from the first and the last ones) has union
+ * operation as a source and shuffle operation as a sink.
+ * Two consecutive stripes should have column fields matching in
+ * shuffle and union operations (so they can be plugged together).
+ *
+ * When Shuffle and Union operations are introduced their fields
+ * respectively `receiversCount` and `source` are left uninitialized.
+ * It's job of the other part of the scheduler to initialize them.
+ */
+vector<query::Operation> stripeOperation(const query::Operation query) {
+  vector<query::Operation> stripes;
+  query::Operation op = query;
+  if (query.has_scan()) {
+    stripes.push_back(op);
+  } else if (query.has_compute()) {
+    stripes = stripeOperation(query.compute().source());
+    query::Operation lastStripe = stripes.back();
+    stripes.pop_back();
+    op.mutable_compute()->clear_source();
+    op.mutable_compute()->mutable_source()->MergeFrom(lastStripe);
+    stripes.push_back(op);
+  } else if (query.has_filter()) {
+    stripes = stripeOperation(query.filter().source());
+    query::Operation lastStripe = stripes.back();
+    stripes.pop_back();
+    op.mutable_filter()->clear_source();
+    op.mutable_filter()->mutable_source()->MergeFrom(lastStripe);
+    stripes.push_back(op);
+  } else if (query.has_group_by()) {
+    stripes = stripeOperation(query.group_by().source());
+    query::Operation lastStripe = stripes.back();
+    stripes.pop_back();
 
-vector<query::Operation>* SchedulerNode::makeStripes(query::Operation query) {
- // TODO : IMPLEMENT THIS
- vector<query::Operation> *stripes = new vector<query::Operation>();
- stripes->push_back(query);
- return stripes;
+    query::Operation shuffle;
+    shuffle.mutable_shuffle()->mutable_source()->MergeFrom(lastStripe);
+    // TODO implement it for real
+    shuffle.mutable_shuffle()->set_receiverscount(10);
+    stripes.push_back(shuffle);
+
+    query::GroupByOperation groupBy = query.group_by();
+
+    // columns needed by group by operations both for keys and values
+    vector<int> columns;
+    for (int i=0; i < groupBy.group_by_column_size(); i++) {
+      columns.push_back(groupBy.group_by_column(i));
+    }
+    for (int i=0; i < groupBy.aggregations_size(); i++) {
+      if (groupBy.aggregations(i).has_aggregated_column())
+        columns.push_back(groupBy.aggregations(i).aggregated_column());
+    }
+
+    groupBy.clear_source();
+    query::UnionOperation* union_ = groupBy.mutable_source()->mutable_union_();
+    for (int i=0; i < columns.size(); i++) {
+      union_->add_column(columns[i]);
+    }
+
+    query::Operation groupByOp;
+    groupByOp.mutable_group_by()->MergeFrom(groupBy);
+    stripes.push_back(groupByOp);
+  } else if (query.has_scan_own() || query.has_union_() || query.has_shuffle() || query.has_final()) {
+    // this should never happen as those nodes are introduced by stripeOperation
+    assert(false);
+  }
+  return stripes;
 }
 
-void SchedulerNode::schedule(vector<query::Operation> *stripe, uint32_t nodes) {
- // TODO : IMPLEMENT THIS
- for (uint32_t i = 1; i <= nodes; i++)
-  sendJob((*stripe)[0], i);
- return ;
+vector<query::Operation>* SchedulerNode::makeStripes(query::Operation query) {
+  vector<query::Operation> stripes = stripeOperation(query);
+  vector<query::Operation> *stripes_ptr = new vector<query::Operation>(stripes.begin(), stripes.end());
+  return stripes_ptr;
+}
+
+/*
+ * Assigns nodes to union operation that is stored in a given stripe. This
+ * method should not be called with first stripe that has no union operations.
+ *
+ * Nodes are specified as a range [nodeStart, nodeEnd).
+ *
+ * This method mutates passed stripe.
+ */
+void assignNodesToUnion(query::Operation& stripe, int nodeStart, int nodeEnd) {
+  if (stripe.has_scan()) {
+    // stripes should never have plain scan
+    assert(false);
+  } else if (stripe.has_compute()) {
+    assignNodesToUnion(*stripe.mutable_compute()->mutable_source(), nodeStart, nodeEnd);
+  } else if (stripe.has_filter()) {
+    assignNodesToUnion(*stripe.mutable_filter()->mutable_source(), nodeStart, nodeEnd);
+  } else if (stripe.has_group_by()) {
+    assignNodesToUnion(*stripe.mutable_group_by()->mutable_source(), nodeStart, nodeEnd);
+  } else if (stripe.has_scan_own()) {
+    // only the first stripe can have scan own operation and this method
+    // should not be called for the first stripe
+  } else if (stripe.has_shuffle()) {
+    assignNodesToUnion(*stripe.mutable_shuffle()->mutable_source(), nodeStart, nodeEnd);
+  } else if (stripe.has_union_()) {
+    for (int i=nodeStart; i<nodeEnd; i++) {
+      stripe.mutable_union_()->add_source(i);
+    }
+  } else if (stripe.has_final()) {
+    assignNodesToUnion(*stripe.mutable_final()->mutable_source(), nodeStart, nodeEnd);
+  }
+}
+
+void assignReceiversCount(query::Operation& stripe, int receiversCount) {
+  // the last operation (the root of stripe) should be shuffle
+  assert(stripe.has_shuffle());
+  stripe.mutable_shuffle()->set_receiverscount(receiversCount);
+}
+
+void SchedulerNode::schedule(vector<query::Operation> *stripes, uint32_t nodes) {
+  int nodesPerStripe = nodes / stripes->size();
+  // we do not hamdle situation when nodes < stripes->size() yet
+  assert(nodesPerStripe > 0);
+  int nodeStart = 1;
+  int nodeEnd = nodeStart + nodesPerStripe;
+  // process the first stripe
+  {
+    query::Operation& firstStripe = stripes->front();
+    assignReceiversCount(firstStripe, nodesPerStripe);
+    for (int i = nodeStart; i < nodeEnd; i++) {
+      sendJob(firstStripe, i);
+    }
+  }
+  for (int i = 1; i < stripes->size()-1; i++) {
+    query::Operation& stripe = (*stripes)[i];
+    // assign nodes to union that were sent jobs to
+    // in a previous iteration
+    assignNodesToUnion(stripe, nodeStart, nodeEnd);
+    nodeStart = 1+i*nodesPerStripe;
+    nodeEnd = 1+(i+1)*nodesPerStripe;
+    assignReceiversCount(stripe, nodesPerStripe);
+    for (int j = nodeStart; j < nodeEnd; i++) {
+      sendJob(stripe, j);
+    }
+  }
+  // schedule the last stripe, use all the remaining workers
+  {
+    query::Operation& lastStripe = stripes->back();
+    assignNodesToUnion(lastStripe, nodeStart, nodeEnd);
+    nodeStart = nodeEnd;
+    nodeEnd = nodes+1;
+    for (int i = nodeStart; i < nodeEnd; i++) {
+      sendJob(lastStripe, i);
+    }
+  }
 }
 
 void SchedulerNode::sendJob(query::Operation &op, uint32_t node) {
@@ -95,6 +235,11 @@ void SchedulerNode::sendJob(query::Operation &op, uint32_t node) {
 void SchedulerNode::run(const query::Operation &op) {
  std::cout << "Scheduling proto: " << op.DebugString() << "\n";
  vector<query::Operation> *stripes = makeStripes(op);
+  std::cout << "after striping: " << std::endl;
+  for (int i=0; i < stripes->size(); i++) {
+    std::cout << "STRIPE " << i << std::endl;
+    std::cout << (*stripes)[i].DebugString() << std::endl;
+  }
  schedule(stripes, nei->nodes_count() - 1);
  delete stripes;
 
