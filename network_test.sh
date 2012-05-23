d1cf51b5a1172555c68c4efed6400169138db1fc
Wojciech Żółtak <wz292583@khaki16.mimuw.edu.pl>
2012-05-23 18:08:07 +0200

    Better logging for network_test.sh .

--- network_test.sh
+++ network_test.sh
@@ -27,17 +27,19 @@ PIDS=""
 for i in `seq 1 $MAX_NUMBER`; do
    PORT=`expr $START_PORT + $i`
   echo "RUNNING:./src/worker $i $PORT $HOSTS $2 &"
-  ./src/worker $i $PORT $HOSTS $2 | tee "logs/$i" & # we will remove query um later
+  ./src/worker $i $PORT $HOSTS $2 > "logs/$i" & # we will remove query um later
   PIDS="$PIDS $!"
 done
 
 echo "RUNNING:./src/scheduler 1 queries/q1.ascii 0 $START_PORT $HOSTS &"
 sleep 1
-./src/scheduler $2 $3 0 $START_PORT $HOSTS | tee "logs/0" &
+./src/scheduler $2 $3 0 $START_PORT $HOSTS > "logs/0" &
 PIDS="$PIDS $!"
 
 echo $PIDS
 sleep 1
+
+cat `find ./logs`
 
 killall worker
 for job in `jobs -p`; do
