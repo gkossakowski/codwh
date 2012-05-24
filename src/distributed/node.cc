70afc3fd0a7f13cb346dbe620655638a6cf9d94d
Wojciech Żółtak <wz292583@students.mimuw.edu.pl>
2012-05-24 10:57:13 +0200

    Fixed intendation and minor cleanup in `distributed/node.*`

--- src/distributed/node.cc
+++ src/distributed/node.cc
@@ -7,62 +7,62 @@
 #include "operators/factory.h"
 
 query::Communication* WorkerNode::getMessage(bool blocking) {
- char *data;
- size_t data_len;
- query::Communication *message;
+  char *data;
+  size_t data_len;
+  query::Communication *message;
 
- if (!blocking) {
-  data = nei->ReadPacketNotBlocking(&data_len);
-  if (NULL == data)
-   return NULL;
- }
- data = nei->ReadPacketBlocking(&data_len);
- message = new query::Communication();
- message->ParseFromString(string(data, data_len));
+  if (!blocking) {
+    data = nei->ReadPacketNotBlocking(&data_len);
+    if (NULL == data)
+      return NULL;
+  }
+  data = nei->ReadPacketBlocking(&data_len);
+  message = new query::Communication();
+  message->ParseFromString(string(data, data_len));
 
- delete[] data;
- return message;
+  delete[] data;
+  return message;
 }
 
 void WorkerNode::parseMessage(query::Communication *message, bool allow_data) {
- if (message->has_operation()) {
-   jobs.push(message->release_operation());
- } else if (message->has_data_request()) {
-   requests.push(message->release_data_request());
- } else if (allow_data && message->has_data_response()){
-  // TODO : consume data
- } else assert(false);
+  if (message->has_operation()) {
+    jobs.push(message->release_operation());
+  } else if (message->has_data_request()) {
+    requests.push(message->release_data_request());
+  } else if (allow_data && message->has_data_response()){
+    // TODO : consume data
+  } else assert(false);
 
- delete message;
+  delete message;
 }
 
 void WorkerNode::getJob() {
- /** Wait until a job occures, than store it in a jobs queue. */
- std::cout << "Awaiting for a job" << std::endl;
- query::Communication *message;
+  /** Wait until a job occures, than store it in a jobs queue. */
+  std::cout << "Awaiting for a job" << std::endl;
+  query::Communication *message;
 
- while (jobs.size() == 0) {
-  message = getMessage(true);
-  parseMessage(message, false); // there should be no incoming data while
-                                // awaiting for a job
- }
- return ;
+  while (jobs.size() == 0) {
+    message = getMessage(true);
+    parseMessage(message, false); // there should be no incoming data while
+                                  // awaiting for a job
+  }
+  return ;
 }
 
 void WorkerNode::getRequest() {
- /** Wait until a data request occures, than store it in a requests queue */
- std::cout << "Awaiting for data request" << std::endl;
- query::Communication *message;
+  /** Wait until a data request occures, than store it in a requests queue */
+  std::cout << "Awaiting for data request" << std::endl;
+  query::Communication *message;
 
- while (requests.size() == 0) {
-  message = getMessage(true);
-  parseMessage(message, true);
- }
- return ;
+  while (requests.size() == 0) {
+    message = getMessage(true);
+    parseMessage(message, true);
+  }
+  return ;
 }
 
 void WorkerNode::setSource(vector<int32_t> source) {
- this->source = source;
+  this->source = source;
 }
 
 vector<Column*> WorkerNode::pull(int32_t number) {
@@ -72,93 +72,93 @@ vector<Column*> WorkerNode::pull(int32_t number) {
 }
 
 void WorkerNode::resetOutput(int buckets, vector<bool> &columns) {
- for (uint32_t i = 0; i < output.size(); i++)
-  if (output[i].size() != 0) assert(false); // calling during processing is forbidden
+  for (uint32_t i = 0; i < output.size(); i++)
+    if (output[i].size() != 0) assert(false); // calling during processing is forbidden
 
- sent_columns.swap(columns);
- output.resize(0); // drop current objects (just in case)
- output.resize(buckets);
- pending_requests.resize(0);
- pending_requests.resize(buckets, 0);
- output_counters.resize(0);
- output_counters.resize(buckets, 0);
- consumers_map.resize(0);
- consumers_map.resize(buckets, -1);
- full_packets = 0;
+  sent_columns.swap(columns);
+  output.resize(0); // drop current objects (just in case)
+  output.resize(buckets);
+  pending_requests.resize(0);
+  pending_requests.resize(buckets, 0);
+  output_counters.resize(0);
+  output_counters.resize(buckets, 0);
+  consumers_map.resize(0);
+  consumers_map.resize(buckets, -1);
+  full_packets = 0;
 }
 
 void WorkerNode::parseRequests() {
- int bucket;
- query::DataRequest *request;
- 
- while (requests.size() > 0) {
-  request = requests.back();
-  requests.pop();
-  bucket = request->stripe() % output.size();
-  consumers_map[bucket] = request->node();
-  pending_requests[bucket] += request->number();
+  int bucket;
+  query::DataRequest *request;
 
-  flushBucket(bucket); // try to send data
-  delete request;
- }
+  while (requests.size() > 0) {
+    request = requests.back();
+    requests.pop();
+    bucket = request->stripe() % output.size();
+    consumers_map[bucket] = request->node();
+    pending_requests[bucket] += request->number();
+
+    flushBucket(bucket); // try to send data
+    delete request;
+  }
 }
 
 void WorkerNode::packData(vector<Column*> &data, int bucket) {
- queue<Packet*> &buck = output[bucket];
+  queue<Packet*> &buck = output[bucket];
 
- if (buck.size() == 0 || buck.back()->full)
-  buck.push(new Packet(data, sent_columns));
- buck.back()->consume(data);
+  if (buck.size() == 0 || buck.back()->full)
+    buck.push(new Packet(data, sent_columns));
+  buck.back()->consume(data);
 
- if (buck.back()->full)
-  full_packets++;
- assert(full_packets <= MAX_OUTPUT_PACKETS);
+  if (buck.back()->full)
+    full_packets++;
+  assert(full_packets <= MAX_OUTPUT_PACKETS);
 
- // read all incoming requests
- query::Communication *message;
- while ((message = getMessage(false)) != NULL) {
-  parseMessage(message, false); // we should have all data already
- }
+  // read all incoming requests
+  query::Communication *message;
+  while ((message = getMessage(false)) != NULL) {
+    parseMessage(message, false); // we should have all data already
+  }
 
- bool flag;
- do {
-  parseRequests();
-  flag = (full_packets == MAX_OUTPUT_PACKETS);
-  if (flag)
-   getRequest();
- } while (flag);
+  bool flag;
+  do {
+    parseRequests();
+    flag = (full_packets == MAX_OUTPUT_PACKETS);
+    if (flag)
+      getRequest();
+  } while (flag);
 
- // finally, try to flush current bucket
- flushBucket(bucket);
+  // finally, try to flush current bucket
+  flushBucket(bucket);
 }
 
 void WorkerNode::flushBucket(int bucket) {
- if (pending_requests[bucket] == 0 || output[bucket].front()->full)
-  return;
- assert(consumers_map[bucket] != -1); // we should know node number from request
+  if (pending_requests[bucket] == 0 || output[bucket].front()->full)
+    return;
+  assert(consumers_map[bucket] != -1); // we should know node number from request
 
- query::Communication com;
- query::DataResponse *response = com.mutable_data_response();
- query::DataPacket *packet;
- const google::protobuf::Reflection *r = response->data().GetReflection();
- string msg;
+  query::Communication com;
+  query::DataResponse *response = com.mutable_data_response();
+  query::DataPacket *packet;
+  const google::protobuf::Reflection *r = response->data().GetReflection();
+  string msg;
 
- response->set_node(nei->my_node_number());
- // send data while we have a full packet and a pending request
- while (pending_requests[bucket] > 0 && !output[bucket].front()->full) {
-  packet = output[bucket].front()->serialize();
-  output_counters[bucket]++; // increase packet number counter
+  response->set_node(nei->my_node_number());
+  // send data while we have a full packet and a pending request
+  while (pending_requests[bucket] > 0 && !output[bucket].front()->full) {
+    packet = output[bucket].front()->serialize();
+    output_counters[bucket]++; // increase packet number counter
 
-  response->set_number(output_counters[bucket]); // set number
-  r->Swap(response->mutable_data(), packet); // set data
-  com.SerializeToString(&msg);
-  nei->SendPacket(consumers_map[bucket], msg.c_str(), msg.size()); // send
+    response->set_number(output_counters[bucket]); // set number
+    r->Swap(response->mutable_data(), packet); // set data
+    com.SerializeToString(&msg);
+    nei->SendPacket(consumers_map[bucket], msg.c_str(), msg.size()); // send
 
-  output[bucket].pop(); // remove packet from queue
-  pending_requests[bucket]--; 
-  full_packets--;
-  delete packet; // dump packet
- }
+    output[bucket].pop(); // remove packet from queue
+    pending_requests[bucket]--;
+    full_packets--;
+    delete packet; // dump packet
+  }
 }
 
 void WorkerNode::sendEof() {
@@ -181,7 +181,7 @@ void WorkerNode::sendEof() {
 
 int WorkerNode::execPlan(query::Operation *op) {
   printf("Worker[%d] job proto tree:\n%s\n",
-      nei->my_node_number(), op->DebugString().c_str());
+         nei->my_node_number(), op->DebugString().c_str());
   Operation* operation = Factory::createOperation(*op);
   FinalOperation* finalOperation = dynamic_cast<FinalOperation*>(operation);
 
@@ -210,7 +210,7 @@ int WorkerNode::execPlan(query::Operation *op) {
         packData((*buckets)[i], i);
       }
     } while (size > 0);
-    
+
     // we have all data; try to send it
     assert(buckets_num > 0);
     for (int i = 0; i < buckets_num; i++) {
@@ -234,13 +234,13 @@ int WorkerNode::execPlan(query::Operation *op) {
 }
 
 void WorkerNode::run() {
- int ret = 0;
- while (0 == ret) {
-  if (0 == jobs.size()) getJob();
-  execPlan(jobs.front());
-  jobs.pop();
- };
- return ;
+  int ret = 0;
+  while (0 == ret) {
+    if (0 == jobs.size()) getJob();
+    execPlan(jobs.front());
+    jobs.pop();
+  };
+  return ;
 }
 /*
  * Cuts query into stripes.
@@ -483,7 +483,7 @@ void SchedulerNode::schedule(vector<query::Operation> *stripes, uint32_t nodes, 
   for (int i = 1; i <= nodesPerStripe; i++) {
     nodeIds.push_back(i);
   }
-  for (int i = nodesPerStripe+1; i <= nodes; i++) {
+  for (uint32_t i = nodesPerStripe+1; i <= nodes; i++) {
     nodeIdsNext.push_back(i);
   }
 
@@ -575,59 +575,59 @@ void SchedulerNode::run(const query::Operation &op) {
 Packet::Packet(vector<Column*> &view, vector<bool> &sent_columns)
   : size(0), full(false)
 {
- size_t row_size = 0;
- columns.resize(view.size(), NULL);
- offsets.resize(view.size(), 0);
- types.resize(view.size(), query::INVALID_TYPE);
- sentColumns = sent_columns;
+  size_t row_size = 0;
+  columns.resize(view.size(), NULL);
+  offsets.resize(view.size(), 0);
+  types.resize(view.size(), query::INVALID_TYPE);
+  sentColumns = sent_columns;
 
- // collect row size and types
- for (uint32_t i = 0; i < view.size(); i++) {
-  types[i] = view[i]->getType();
-  if (sentColumns[i]){
-    row_size += global::getTypeSize(types[i]);
-  } // otherwise we omit it
- }
+  // collect row size and types
+  for (uint32_t i = 0; i < view.size(); i++) {
+    types[i] = view[i]->getType();
+    if (sentColumns[i]){
+      row_size += global::getTypeSize(types[i]);
+    } // otherwise we omit it
+  }
 
- // compute maximum capacity
- capacity = MAX_PACKET_SIZE / row_size;
+  // compute maximum capacity
+  capacity = MAX_PACKET_SIZE / row_size;
 
- // allocate space and reset data counters;
- for (uint32_t i = 0; i < view.size(); i++)
-  if (sent_columns[i])
-   columns[i] = new char[capacity * global::getTypeSize(types[i])];
+  // allocate space and reset data counters;
+  for (uint32_t i = 0; i < view.size(); i++)
+    if (sent_columns[i])
+      columns[i] = new char[capacity * global::getTypeSize(types[i])];
 }
 
 /** Tries to consume a view. If it's too much data - returns false. */
 void Packet::consume(vector<Column*> view){
- if (full) assert(false); // should check if it's full before calling!
+  if (full) assert(false); // should check if it's full before calling!
 
- for (uint32_t i = 0; i < view.size(); i++)
-  if (sentColumns[i])
-   offsets[i] += view[i]->transfuse(columns[i] + offsets[i]);
- size += static_cast<size_t>(view[0]->size);
+  for (uint32_t i = 0; i < view.size(); i++)
+    if (sentColumns[i])
+      offsets[i] += view[i]->transfuse(columns[i] + offsets[i]);
+  size += static_cast<size_t>(view[0]->size);
 
- if (capacity - size < static_cast<size_t>(DEFAULT_CHUNK_SIZE))
-  full = true;
+  if (capacity - size < static_cast<size_t>(DEFAULT_CHUNK_SIZE))
+    full = true;
 }
 
 query::DataPacket* Packet::serialize() {
- query::DataPacket *packet = new query::DataPacket();
+  query::DataPacket *packet = new query::DataPacket();
 
- for (uint32_t i = 0; i < types.size(); i++) {
-  packet->add_type(abs(types[i]));
-  if (sentColumns[i])
-   packet->add_data(columns[i], offsets[i]);
-  else packet->add_data(string()); // empty string
- }
- return packet;
+  for (uint32_t i = 0; i < types.size(); i++) {
+    packet->add_type(abs(types[i]));
+    if (sentColumns[i])
+      packet->add_data(columns[i], offsets[i]);
+    else packet->add_data(string()); // empty string
+  }
+  return packet;
 }
 
 Packet::Packet(char data[], size_t data_len) {
- // TODO : IMPLEMENT THIS
+  // TODO : IMPLEMENT THIS
 }
 
 Packet::~Packet() {
- for(uint32_t i = 0; i < columns.size(); i++)
-  delete[] columns[i];
+  for(uint32_t i = 0; i < columns.size(); i++)
+    delete[] columns[i];
 }
--- src/distributed/node.h
+++ src/distributed/node.h
@@ -17,108 +17,103 @@ using std::vector;
 class Packet;
 
 class WorkerNode {
- private:
-  WorkerNode(const WorkerNode &node){};
+  private:
+    WorkerNode(const WorkerNode &node){};
 
- protected:
-  vector<int32_t> source;
-  NodeEnvironmentInterface *nei;
-  queue<query::Operation *> jobs;
-  queue<query::DataRequest *> requests;
+  protected:
+    vector<int32_t> source;
+    NodeEnvironmentInterface *nei;
+    queue<query::Operation *> jobs;
+    queue<query::DataRequest *> requests;
 
-  /** Input buffer (stack) */
-  vector<Column*> input;
+    /** Input buffer (stack) */
+    vector<Column*> input;
 
-  /** Output buffer */
-  int full_packets;
-  char *output_data;
-  vector< queue<Packet*> > output;
-  vector<int> pending_requests;
-  vector<int> output_counters;
-  vector<int> consumers_map;
-  vector<bool> sent_columns;
+    /** Output buffer */
+    int full_packets;
+    vector< queue<Packet*> > output;
+    vector<int> pending_requests;
+    vector<int> output_counters;
+    vector<int> consumers_map;
+    vector<bool> sent_columns;
 
-  /** Execute a first plan from a jobs queue */
-  int execPlan(query::Operation *op);
+    /** Execute a first plan from a jobs queue */
+    int execPlan(query::Operation *op);
 
-  /** Gets a communication message from network interface */
-  query::Communication* getMessage(bool blocking);
-  /** Parses a communication method and stores contained information */
-  void parseMessage(query::Communication *message, bool allow_data);
-  /** Reads a data request from queue, tries to satisfy the consumer and
-   *  schedule job for later if it's not possible. */
-  void parseRequests();
+    /** Gets a communication message from network interface */
+    query::Communication* getMessage(bool blocking);
+    /** Parses a communication method and stores contained information */
+    void parseMessage(query::Communication *message, bool allow_data);
+    /** Reads a data request from queue, tries to satisfy the consumer and
+     *  schedule job for later if it's not possible. */
+    void parseRequests();
 
-  /** Wait until any job occurs */
-  void getJob();
-  /** Wait until any data request occurs */
-  void getRequest();
+    /** Wait until any job occurs */
+    void getJob();
+    /** Wait until any data request occurs */
+    void getRequest();
 
- public:
-  WorkerNode(NodeEnvironmentInterface *nei)
-  : nei(nei), output_data(new char[MAX_PACKET_SIZE + 100]) {};
+    /** Tries to send accumulated data to a consumer */
+    void flushBucket(int bucket);
+    /** Sends EOF messages to consumers */
+    void sendEof();
 
-  /** Set data sources */
-  void setSource(vector<int32_t> source);
-  /** Get data from any source */
-  vector<Column*> pull(int32_t number);
+  public:
+    WorkerNode(NodeEnvironmentInterface *nei) : nei(nei) {};
 
-  /** Reset output buffer for a given number of buckets and a boolmask *
-   *  of sent columns.                                                 */
-  void resetOutput(int buckets, vector<bool> &columns);
-  /** Pack given data and eventually send to a consumer. Blocks if buffer is *
-    * full.                                                                  */
-  void packData(vector<Column*> &data, int bucket);
-  /** Tries to send accumulated data to a consumer */
-  void flushBucket(int bucket);
-  /** Sends EOF messages to consumers */
-  void sendEof();
+    /** Set data sources */
+    void setSource(vector<int32_t> source);
+    /** Get data from any source */
+    vector<Column*> pull(int32_t number);
 
-  /** Run worker */
-  virtual void run();
+    /** Reset output buffer for a given number of buckets and a boolmask *
+     *  of sent columns.                                                 */
+    void resetOutput(int buckets, vector<bool> &columns);
+    /** Pack given data and eventually send to a consumer. Blocks if buffer is *
+      * full. */
+    void packData(vector<Column*> &data, int bucket);
 
-  ~WorkerNode(){
-    delete[] output_data;
-  }
+    /** Run worker */
+    virtual void run();
 };
 
 class SchedulerNode : public WorkerNode {
- private:
-  SchedulerNode(const SchedulerNode &node);
+  private:
+    SchedulerNode(const SchedulerNode &node);
 
- protected:
-  /** Slice query into stripes */
-  vector<query::Operation>* makeStripes(query::Operation query);
-  /** Fill in and send stripes for a given number of nodes */
-  void schedule(vector<query::Operation> *stripe, uint32_t nodes, int numberOfFiles);
-  /** Send job to a given node, the given job object is destroyed */
-  void sendJob(query::Operation &op, uint32_t node, int stripeId);
+  protected:
+    /** Slice query into stripes */
+    vector<query::Operation>* makeStripes(query::Operation query);
+    /** Fill in and send stripes for a given number of nodes */
+    void schedule(vector<query::Operation> *stripe, uint32_t nodes, int numberOfFiles);
+    /** Send job to a given node, the given job object is destroyed */
+    void sendJob(query::Operation &op, uint32_t node, int stripeId);
 
- public:
-  SchedulerNode(NodeEnvironmentInterface *nei) : WorkerNode(nei) {};
+   public:
+    SchedulerNode(NodeEnvironmentInterface *nei) : WorkerNode(nei) {};
 
-  /** Run scheduler */
-  virtual void run(const query::Operation &op);
+    /** Run scheduler */
+    virtual void run(const query::Operation &op);
 };
 
 class Packet {
- private:
-  vector<char *> columns;
-  vector<uint32_t> offsets;
-  vector<query::ColumnType> types;
-  vector<bool> sentColumns;
-  size_t size; /** size in rows */
-  size_t capacity; /** maximum capacity in rows */
+  private:
+    vector<char *> columns;
+    vector<uint32_t> offsets;
+    vector<query::ColumnType> types;
+    vector<bool> sentColumns;
+    size_t size; /** size in rows */
+    size_t capacity; /** maximum capacity in rows */
 
- public:
-  bool full; /** only Packet should write to this! */
-  Packet(vector<Column*> &view, vector<bool> &sent_columns);
-  Packet(char data[], size_t data_len);
+  public:
+    bool full; /** only Packet should write to this! */
+    Packet(vector<Column*> &view, vector<bool> &sent_columns);
+    Packet(char data[], size_t data_len);
 
-  void consume(vector<Column*> view);
-  query::DataPacket* serialize();
+    void consume(vector<Column*> view);
+    query::DataPacket* serialize();
 
-  ~Packet();
+    ~Packet();
 };
 
 #endif // DISTRIBUTED_NODE_H
